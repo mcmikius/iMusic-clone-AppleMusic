1d774be8cc10a8b78968ab6387ebaf18722b5b67
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-02-18 16:52:16 +0200

    add image

--- iAppleMusic/iAppleMusic/Search/Cell/TrackCell.swift
+++ iAppleMusic/iAppleMusic/Search/Cell/TrackCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SDWebImage
 
 protocol TrackCellViewModel {
     var iconUrlString: String? { get }
@@ -28,9 +29,18 @@ class TrackCell: UITableViewCell {
         super.awakeFromNib()
     }
     
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        trackImageView.image = nil
+    }
+    
     func set(viewModel: TrackCellViewModel) {
         trackNameLabel.text = viewModel.trackName
         artistNameLabel.text = viewModel.artistName
         collectionNameLabel.text = viewModel.collectionName
+        guard let url = URL(string: viewModel.iconUrlString ?? "") else {
+            return
+        }
+        trackImageView.sd_setImage(with: url, completed: nil)
     }
 }
