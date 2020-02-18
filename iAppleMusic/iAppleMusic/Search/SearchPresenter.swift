dbd259625eb0d274cf4abc0fb087cdaed5a03d29
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-02-18 15:36:40 +0200

    full fucking mother fucker Clean Swift

--- iAppleMusic/iAppleMusic/Search/SearchPresenter.swift
+++ iAppleMusic/iAppleMusic/Search/SearchPresenter.swift
@@ -16,7 +16,13 @@ class SearchPresenter: SearchPresentationLogic {
   weak var viewController: SearchDisplayLogic?
   
   func presentData(response: Search.Model.Response.ResponseType) {
-  
+    switch response {
+    case .some:
+        print("presenter .some")
+    case .presentTracks:
+        print("presenter .presentTracks")
+        viewController?.displayData(viewModel: Search.Model.ViewModel.ViewModelData.displayTracks)
+    }
   }
   
 }
--- iAppleMusic/iAppleMusic/Search/SearchViewController.swift
+++ iAppleMusic/iAppleMusic/Search/SearchViewController.swift
@@ -9,57 +9,64 @@
 import UIKit
 
 protocol SearchDisplayLogic: class {
-  func displayData(viewModel: Search.Model.ViewModel.ViewModelData)
+    func displayData(viewModel: Search.Model.ViewModel.ViewModelData)
 }
 
 class SearchViewController: UIViewController, SearchDisplayLogic {
-
-  var interactor: SearchBusinessLogic?
-  var router: (NSObjectProtocol & SearchRoutingLogic)?
-
+    
+    var interactor: SearchBusinessLogic?
+    var router: (NSObjectProtocol & SearchRoutingLogic)?
+    
     @IBOutlet weak var table: UITableView!
     
     let searchController = UISearchController(searchResultsController: nil)
-  
-  // MARK: Setup
-  
-  private func setup() {
-    let viewController        = self
-    let interactor            = SearchInteractor()
-    let presenter             = SearchPresenter()
-    let router                = SearchRouter()
-    viewController.interactor = interactor
-    viewController.router     = router
-    interactor.presenter      = presenter
-    presenter.viewController  = viewController
-    router.viewController     = viewController
-  }
-  
-  // MARK: Routing
-  
-
-  
-  // MARK: View lifecycle
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    setup()
-    setupSearchBar()
-    setupTableView()
-  }
-  
+    
+    // MARK: Setup
+    
+    private func setup() {
+        let viewController        = self
+        let interactor            = SearchInteractor()
+        let presenter             = SearchPresenter()
+        let router                = SearchRouter()
+        viewController.interactor = interactor
+        viewController.router     = router
+        interactor.presenter      = presenter
+        presenter.viewController  = viewController
+        router.viewController     = viewController
+    }
+    
+    // MARK: Routing
+    
+    
+    
+    // MARK: View lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+        setupSearchBar()
+        setupTableView()
+    }
+    
     private func setupSearchBar() {
         navigationItem.searchController = searchController
         navigationItem.hidesSearchBarWhenScrolling = false
         searchController.searchBar.delegate = self
     }
+    
     private func setupTableView() {
         table.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
     }
-  func displayData(viewModel: Search.Model.ViewModel.ViewModelData) {
-
-  }
-  
+    
+    func displayData(viewModel: Search.Model.ViewModel.ViewModelData) {
+        switch viewModel {
+        case .some:
+            print("viewController .some")
+        case .displayTracks:
+            print("viewController .displayTracks")
+        }
+    }
+    
 }
 
 extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
@@ -81,5 +88,6 @@ extension SearchViewController: UISearchBarDelegate {
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         print(searchText)
+        interactor?.makeRequest(request: Search.Model.Request.RequestType.some)
     }
 }
