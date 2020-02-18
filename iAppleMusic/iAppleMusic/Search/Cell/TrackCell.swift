b73d0da588ffe2bda37bb36498bc9a9b38e24567
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-02-18 16:31:49 +0200

    bug

---
+++ iAppleMusic/iAppleMusic/Search/Cell/TrackCell.swift
@@ -0,0 +1,25 @@
+//
+//  TrackCell.swift
+//  iAppleMusic
+//
+//  Created by Mykhailo Bondarenko on 18.02.2020.
+//  Copyright © 2020 Mykhailo Bondarenko. All rights reserved.
+//
+
+import UIKit
+
+class TrackCell: UITableViewCell {
+    
+    static let reuseId = "TrackCell"
+    
+    @IBOutlet weak var trackImageView: UIImageView!
+    @IBOutlet weak var trackNameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var collectionNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func set
+}
--- iAppleMusic/iAppleMusic/Search/SearchViewController.swift
+++ iAppleMusic/iAppleMusic/Search/SearchViewController.swift
@@ -58,7 +58,9 @@ class SearchViewController: UIViewController, SearchDisplayLogic {
     }
     
     private func setupTableView() {
-        table.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+//        table.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        let nib = UINib(nibName: "TrackCell", bundle: nil)
+        table.register(nib, forCellReuseIdentifier: TrackCell.reuseId)
     }
     
     func displayData(viewModel: Search.Model.ViewModel.ViewModelData) {
@@ -81,14 +83,18 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = table.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        let cell = table.dequeueReusableCell(withIdentifier: TrackCell.reuseId, for: indexPath) as! TrackCell
         let cellViewModel = searchViewModel.cells[indexPath.row]
-        cell.textLabel?.text = "\(cellViewModel.trackName)\n\(cellViewModel.artistName)"
-        cell.textLabel?.numberOfLines = 2
-        cell.imageView?.image = UIImage(named: "pic0")
+        cell.trackImageView.backgroundColor = .lightGray
+//        cell.textLabel?.text = "\(cellViewModel.trackName)\n\(cellViewModel.artistName)"
+//        cell.textLabel?.numberOfLines = 2
+//        cell.imageView?.image = UIImage(named: "pic0")
         return cell
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 84
+    }
     
 }
 
