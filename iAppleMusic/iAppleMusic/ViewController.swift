5ccc24620c5d71a67d1cff43d17cb51472fc4533
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-02-18 12:52:37 +0200

    init

---
+++ iAppleMusic/iAppleMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iAppleMusic
+//
+//  Created by Mykhailo Bondarenko on 18.02.2020.
+//  Copyright © 2020 Mykhailo Bondarenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
