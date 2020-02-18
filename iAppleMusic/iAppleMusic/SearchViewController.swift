394ff649e528ebee3ea29d094e91804249d82095
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-02-18 13:37:16 +0200

    add model for track custom cell add searchController

--- iAppleMusic/iAppleMusic/SearchViewController.swift
+++ iAppleMusic/iAppleMusic/SearchViewController.swift
@@ -9,21 +9,41 @@
 import UIKit
 
 class SearchViewController: UITableViewController {
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    let tracks = [TrackModel(trackName: "bad guy", artistName: "Billie Eilish"), TrackModel(trackName: "Help!", artistName: "theBeatles")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        
+        setupSearchBar()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
         
     }
     
+    private func setupSearchBar() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.delegate = self
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return tracks.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
-        cell.textLabel?.text = "indexPath: \(indexPath)"
+        let track = tracks[indexPath.row]
+        cell.textLabel?.text = "\(track.trackName)\n\(track.artistName)"
+        cell.textLabel?.numberOfLines = 2
+        cell.imageView?.image = UIImage(named: "pic0")
         return cell
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
     }
 }
---
+++ iAppleMusic/iAppleMusic/TrackModel.swift
@@ -0,0 +1,14 @@
+//
+//  TrackModel.swift
+//  iAppleMusic
+//
+//  Created by Mykhailo Bondarenko on 18.02.2020.
+//  Copyright © 2020 Mykhailo Bondarenko. All rights reserved.
+//
+
+import Foundation
+
+struct TrackModel {
+    var trackName: String
+    var artistName: String
+}
