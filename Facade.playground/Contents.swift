518ffac294eaa72253353b52c414d1fe399a196f
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-02-19 08:54:32 +0200

    start classes

--- Facade.playground/Contents.swift
+++ Facade.playground/Contents.swift
@@ -1,1 +1,26 @@
 
+class FruitShop {
+    func buyFruits() -> String {
+        return "fruits"
+    }
+}
+class MeatShop {
+    func buyMeat() -> String {
+        return "meat"
+    }
+}
+class MilkShop {
+    func buyMilk() -> String {
+        return "milk"
+    }
+}
+class SweetShop {
+    func buySweets() -> String {
+        return "sweets"
+    }
+}
+class BreadShop {
+    func buyBread() -> String {
+        return "bread"
+    }
+}
