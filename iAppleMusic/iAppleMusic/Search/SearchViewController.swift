144937423e70e57c00cef5beab66c02d2525f79d
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-02-19 13:43:23 +0200

    add layout

--- iAppleMusic/iAppleMusic/Search/SearchViewController.swift
+++ iAppleMusic/iAppleMusic/Search/SearchViewController.swift
@@ -94,6 +94,7 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let cellViewModel = searchViewModel.cells[indexPath.row]
+        // 'keyWindow' was deprecated in iOS 13.0: Should not be used for applications that support multiple scenes as it returns a key window across all connected scenes
         let window = UIApplication.shared.connectedScenes.filter({$0.activationState == .foregroundActive}).map({$0 as? UIWindowScene}).compactMap({$0}).first?.windows.filter({$0.isKeyWindow}).first
         let trackDetailsView = Bundle.main.loadNibNamed("TrackDetailView", owner: self, options: nil)?.first as! TrackDetailView
         window?.addSubview(trackDetailsView)
--- iAppleMusic/iAppleMusic/Search/TrackDetailView/TrackDetailView.swift
+++ iAppleMusic/iAppleMusic/Search/TrackDetailView/TrackDetailView.swift
@@ -10,7 +10,31 @@ import UIKit
 
 class TrackDetailView: UIView {
     
+    @IBOutlet weak var trackImageView: UIImageView!
+    @IBOutlet weak var currentTimeSlider: UISlider!
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    @IBOutlet weak var durationTimeLabel: UILabel!
+    @IBOutlet weak var trackTitleLabel: UILabel!
+    @IBOutlet weak var authorTitleLabel: UILabel!
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var volumeSlider: UISlider!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        trackImageView.backgroundColor = .red
+    }
+    @IBAction func dragDownButtonTapped(_ sender: UIButton) {
+        self.removeFromSuperview()
+    }
+    @IBAction func handleCurrentTimerSlider(_ sender: UISlider) {
+    }
+    @IBAction func handleVolumeSlider(_ sender: UISlider) {
+    }
+    @IBAction func previousTrack(_ sender: UIButton) {
+    }
+    @IBAction func nextTrack(_ sender: UIButton) {
+    }
+    @IBAction func playPauseAction(_ sender: UIButton) {
     }
 }
